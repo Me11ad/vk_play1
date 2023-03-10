8c678f117c5d9910ec8fba8e4609adaec0f59465
Me11ad <112916199+Me11ad@users.noreply.github.com>
2023-03-10 12:39:03 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+const bodyParser = require("body-parser");
 const express = require("express");
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database('./db/likesteam.db')
 
 var app = express();
+app.use(bodyParser.urlencoded())
+app.use(bodyParser.json())
 
 
 app.listen(8000, () => {
@@ -11,4 +14,17 @@ app.listen(8000, () => {
 
 app.get('/', (req, res) => {
     res.json('Молодец!')
+})
+
+app.get('/games', (req, res) => {
+    db.all(`SELECT * FROM GAMES`,(err, rows) => {
+
+    })
+})
+
+app.get('/games/price/:price', (req, res) => {
+    const price = req.params.price
+    db.all(`SELECT * FROM GAMES WHERE price > $price`, {$price: price} ,(err, rows) => {
+        res.json(rows)
+    })
 })
